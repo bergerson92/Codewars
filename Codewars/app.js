7eba2af0d885e056bcc005ed6ba08d8459768cb0
bergerson92 <90724387+bergerson92@users.noreply.github.com>
2023-10-08 15:04:19 +0200

    Update app.js

--- Codewars/app.js
+++ Codewars/app.js
@@ -29,6 +29,7 @@ let removeLast = removeFirst.slice(0, removeFirst.length -1);
 return removeLast;
 };
 
+// new challenge
 function feast(beast, dish) {
   return beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length - 1) === dish.charAt(dish.length - 1);
 }
@@ -37,3 +38,15 @@ function feast(beast, dish) {
 function removeExclamationMarks(s) {
   return s.replace(/!/g,"");
 }
+
+// new challenge
+function firstNonConsecutive (arr) {
+  for(let i = 1; i<arr.length; i++){
+    if(arr[i] !== arr[i -1] +1)
+      {
+        return arr[i];
+      }
+  }
+  return null;
+}
+
