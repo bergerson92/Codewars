79925a86356b3433ecd809899b01ad72651fe7c2
bergerson92 <90724387+bergerson92@users.noreply.github.com>
2023-10-18 19:52:10 +0200

    Update app.js

--- Codewars/app.js
+++ Codewars/app.js
@@ -152,3 +152,23 @@ function getFirstNonRepeating(s) {
     }
     return " ";
 }
+
+// new Challenge
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
+
+function GetSum(a,b)
+{
+  return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+function GetSum( a,b )
+{
+   if (a == b) return a;
+   else if (a < b) return a + GetSum(a+1, b);
+   else return a + GetSum(a-1,b);
+}
+
